0f08b1ad97b921ebb8de74f6696fd6d64616b5af
DMN-sohan <65285526+DMN-sohan@users.noreply.github.com>
2025-05-03 01:37:55 +0530

    NO UNET; MLP

--- train.py
+++ train.py
@@ -53,8 +53,12 @@ def main():
 
     # encoder = model.StegaStampEncoder()   8.1.2023
 
-    encoder = model.StegaStampEncoderUnet(KAN=args.KAN)
-    decoder = model.StegaStampDecoderUnet(secret_size=args.secret_size, KAN=args.KAN)
+    
+    encoder = model.StegaStampEncoder()
+    decoder = model.StegaStampDecoder(secret_size=args.secret_size)
+
+    # encoder = model.StegaStampEncoderUnet(KAN=args.KAN)
+    # decoder = model.StegaStampDecoderUnet(secret_size=args.secret_size, KAN=args.KAN)
 
     discriminator = model.Discriminator()
     lpips_alex = lpips.LPIPS(net="alex", verbose=False)
@@ -108,8 +112,8 @@ def main():
             if args.cuda:
                 Ms = Ms.cuda()
 
-            loss_scales = [l2_loss_scale, lpips_loss_scale, secret_loss_scale, G_loss_scale]
-            # loss_scales = [l2_loss_scale, 0, secret_loss_scale, 0]
+            # loss_scales = [l2_loss_scale, lpips_loss_scale, secret_loss_scale, G_loss_scale]
+            loss_scales = [l2_loss_scale, 0, secret_loss_scale, 0]
             yuv_scales = [args.y_scale, args.u_scale, args.v_scale]
             loss, secret_loss, D_loss, bit_acc, str_acc = model.build_model(encoder, decoder, discriminator, lpips_alex,
                                                                             secret_input, image_input,
